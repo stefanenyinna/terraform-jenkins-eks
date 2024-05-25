5f0a85069c3d7dd38b40ca75f1258eaa8c55c400
stefanenyinna <97253020+stefanenyinna@users.noreply.github.com>
2024-05-25 12:11:31 -0700

    FOund a bug

--- EKS/main.tf
+++ EKS/main.tf
@@ -36,11 +36,8 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
-
-
-  vpc_id     = module.vpc.default_vpc_id
-  subnet_ids = module.vpc.private_subnets
-
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets   
   eks_managed_node_groups = {
     nodes = {
       min_size     = 1
